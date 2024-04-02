ddb785cfd44d54e98a079ed85151df04b5031739
Andrei Kozel <kozel.andrei.94@gmail.com>
2024-04-02 21:15:09 +0200

    Add unit tests for the Shorten function in shorten_test.go

--- internal/shorten/shorten_test.go
+++ internal/shorten/shorten_test.go
@@ -1,4 +1,4 @@
-package shorten
+package shorten_test
 
 import (
 	"testing"
@@ -17,13 +17,19 @@ func TestShorten(t *testing.T) {
 		testCases := []testCase{
 			{id: 1024, expected: "Mv"},
 			{id: 0, expected: ""},
-			{id: 1, expected: "y"},
+			{id: 1, expected: "n"},
 		}
 
 		for _, tc := range testCases {
 			actual := shorten.Shorten(tc.id)
 			assert.Equal(t, tc.expected, actual)
 		}
+
 	})
-	t.Run("Is identical for the same input", func(t *testing.T) {})
+
+	t.Run("Is identical for the same input", func(t *testing.T) {
+		for i := 0; i < 100; i++ {
+			assert.Equal(t, "Mv", shorten.Shorten(1024))
+		}
+	})
 }
